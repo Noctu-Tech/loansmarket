bfd6833836cd2a4554af3b6f5b16e99ba55c5d0a
Nmait2111 <namitjain2111@gmail.com>
2024-09-21 14:27:09 +0530

    service page done

--- frontend/src/routes/AllRoutes.jsx
+++ frontend/src/routes/AllRoutes.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from '../pages/Home';
 import EMICalculator from '../pages/EMICalculator';
 import ApplyForm from '../pages/ApplyForm';
+import ServicesPage from '../pages/ServicesPage';
 const AllRoutes = () => {
 
   return (
@@ -13,6 +14,9 @@ const AllRoutes = () => {
             <Route path='/' element={<Home/>}/>
             <Route path ="/emi" element={<EMICalculator/>}/>
             <Route path="/apply" element={<ApplyForm/>} />
+            <Route path="/services" element={
+          <ServicesPage/>
+            } />
         </Routes>
     </Router>
   )
